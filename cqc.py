8900c0f5037d012d4ebf844b05e6ba5020271501
jgrugru <jeff.gruenbaum@gmail.com>
2024-08-22 14:49:05 -0500

    adding rich console text

--- cqc.py
+++ cqc.py
@@ -1,20 +1,27 @@
 import subprocess
 import sys
 
+from rich.console import Console
+
+console = Console()
+
 
 def run_isort():
+    console.print("[bold green]Running isort[/bold green]! :avocado:")
     result = subprocess.run(["isort", ".", "--line-length", "255"])
     if result.returncode != 0:
         sys.exit(result.returncode)
 
 
 def run_black():
+    console.print("[bold green]Running black[/bold green]! :broccoli:")
     result = subprocess.run(["black", ".", "--line-length", "255"])
     if result.returncode != 0:
         sys.exit(result.returncode)
 
 
 def run_ruff():
+    console.print("[bold green]Running ruff[/bold green]! :green_apple:")
     result = subprocess.run(["ruff", "check", ".", "--line-length", "255"])
     if result.returncode != 0:
         sys.exit(result.returncode)
@@ -24,3 +31,4 @@ def main():
     run_isort()
     run_black()
     run_ruff()
+    console.print("[bold green]Don't forget to eat your veggies. - Anonymous[/bold green] :cucumber: :carrot: :onion: :watermelon:")
